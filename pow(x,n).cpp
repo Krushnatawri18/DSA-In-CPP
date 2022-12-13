321d7dfaba29d21907cd5d75f76e7e7c73aa8231
Krushna Tawri <116067639+Krushnatawri18@users.noreply.github.com>
2022-12-13 18:42:02 +0530

    Create pow(x,n).cpp

---
+++ pow(x,n).cpp
@@ -0,0 +1,19 @@
+
+/* Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+*/
+
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=pow(x,n);
+        
+        return ans;
+    }
+  
+};
