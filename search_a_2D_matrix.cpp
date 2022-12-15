5806810ac2255ed3a19a61035e54b7f356d7146f
Krushna Tawri <116067639+Krushnatawri18@users.noreply.github.com>
2022-12-15 13:20:42 +0530

    Update search_a_2D_matrix.cpp

--- search_a_2D_matrix.cpp
+++ search_a_2D_matrix.cpp
@@ -1,6 +1,7 @@
 # cpp
 
-Search a 2D Matrix
+/*
+ Search a 2D Matrix
 
 Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
 
@@ -11,6 +12,7 @@ Example 1:
 
 Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
 Output: true
+*/
 
 class Solution {
 public:
