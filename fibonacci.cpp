a1fa94dac51a78b31340fb5208f6ed4f9bb65d75
Krushna Tawri <116067639+Krushnatawri18@users.noreply.github.com>
2022-12-19 18:20:37 +0530

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,41 @@
+# DSA_in_cpp
+
+/*
+
+Fibonacci Number
+
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Runtime 19 ms  Memory 5.9 MB
+
+*/
+
+class Solution {
+public:
+    int fib(int n) {
+        
+        // base case 
+        if(n == 0){
+            return 0;
+        }
+
+        if(n == 1){
+            return 1;
+        }
+
+        // recursive relation
+        int ans = fib(n-1) + fib(n-2);
+        
+    return ans;
+    }
+};
