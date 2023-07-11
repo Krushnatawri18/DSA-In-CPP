29b53643bdd937ae4a7bea9589442f2311ff998d
Krushna Tawri <116067639+Krushnatawri18@users.noreply.github.com>
2023-07-11 12:31:53 +0530

    Update Sort 0s,1s and 2s in Linked list.cpp

--- Sort 0s,1s and 2s in Linked list.cpp
+++ Sort 0s,1s and 2s in Linked list.cpp
@@ -62,3 +62,71 @@ class Solution
     return head;
     }
 };
+
+/*
+TC - O(N)
+SC - O(N)
+*/
+
+class Solution
+{
+    public:
+    
+    void insertAtTail(Node* &tail, Node* curr ) {
+    tail -> next = curr;
+    tail = curr;
+    }
+
+Node* segregate(Node *head) {
+
+    Node* zeroHead = new Node(-1);
+    Node* zeroTail = zeroHead;
+    Node* oneHead = new Node(-1);
+    Node* oneTail = oneHead;
+    Node* twoHead = new Node(-1);
+    Node* twoTail = twoHead;
+    
+    Node* curr = head;
+    
+    // creating separate linked list for 0,1 and 2
+    while(curr != NULL) {
+        
+        int value = curr -> data;
+        
+        if(value == 0) {
+            insertAtTail(zeroTail, curr);
+        }
+        else if(value == 1) {
+            insertAtTail(oneTail, curr);
+        }
+        else if(value == 2) {
+            insertAtTail(twoTail, curr);
+        }       
+        curr = curr -> next;
+    }
+    
+    //merge 3 sublist
+    
+    // if 1s list is empty
+    if(oneHead -> next != NULL) {
+        zeroTail -> next = oneHead -> next;
+    }
+    else {
+        zeroTail -> next = twoHead -> next;
+    }
+    
+    oneTail -> next = twoHead -> next;
+    twoTail -> next = NULL;
+
+	// setting new head
+    head = zeroHead -> next;
+    
+    // deleting dummy nodes
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+    
+    return head;
+    
+    }
+};
